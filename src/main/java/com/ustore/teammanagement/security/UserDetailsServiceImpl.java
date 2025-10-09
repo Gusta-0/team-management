d1e4fff6e56a4aecb8db3ba920d2e882e7c18989
GUSTAVO ALVES BARBOSA DA SILVA <gustavoalves.silva@a.ficr.edu.br>
2025-10-09 19:14:14 -0300

    Adicionando camada de segurança

---
+++ src/main/java/com/ustore/teammanagement/security/UserDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package com.ustore.teammanagement.security;
+
+import com.ustore.gerenciamentoequipes.core.entity.Usuario;
+import com.ustore.gerenciamentoequipes.core.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Usuario usuario = usuarioRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado: " + email));
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(usuario.getEmail())
+                .password(usuario.getPassword())
+                .build();
+    }
+}
