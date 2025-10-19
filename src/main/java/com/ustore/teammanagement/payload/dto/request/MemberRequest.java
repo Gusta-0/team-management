71d573a945a8764663bee333476b76f81dfcc510
Gustavo Alves <gabs.principal.2005@gmail.com>
2025-10-19 12:28:53 -0300

    Adicionado role como atributo de MemberRequest

--- src/main/java/com/ustore/teammanagement/payload/dto/request/MemberRequest.java
+++ src/main/java/com/ustore/teammanagement/payload/dto/request/MemberRequest.java
@@ -5,7 +5,7 @@ import jakarta.validation.constraints.*;
 
 public record MemberRequest(
         @NotBlank(message = "Nome é obrigatório")
-        @Size(min = 2, max = 100, message = "Nome deve ter entre 2 e 100 caracteres")
+        @Size(min = 8, max = 100, message = "Nome deve ter entre 2 e 100 caracteres")
         String name,
 
         @NotBlank(message = "Email é obrigatório")
