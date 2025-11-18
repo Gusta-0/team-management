d544d3bea1916a8ba66205a9f0b2241b4f4706c9
Gustavo Alves <gabs.principal.2005@gmail.com>
2025-11-18 10:31:26 -0300

    refactor(exception): remover exceções não utilizadas

--- src/main/java/com/ustore/teammanagement/exceptions/ConflictException.java
+++ src/main/java/com/ustore/teammanagement/exceptions/ConflictException.java
@@ -4,8 +4,4 @@ public class ConflictException extends RuntimeException {
     public ConflictException(String mensagem){
         super(mensagem);
     }
-
-    public ConflictException(String mensagem, Throwable throwable){
-        super(mensagem);
-    }
 }
